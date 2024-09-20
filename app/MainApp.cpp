27346a1b6d839581a9999dd527746e9d6f68524d
yli <79628285@qq.com>
2024-09-20 10:20:38 +0800

    add mqtt test

--- app/MainApp.cpp
+++ app/MainApp.cpp
@@ -5,29 +5,70 @@
 #include <chrono>
 #include <iostream>
 #include <fstream>
-#include "Utility.h"
-
-#include "document.h"
-#include "stringbuffer.h"
-#include "writer.h"
 #include <thread>
 #include <chrono>
-
-#include <openssl/md5.h>
 #include <iomanip>
 #include <sstream>
 
-#include "Base.h"
-#include "Version.h"
 
-using namespace rapidjson;
+#include <iostream>
+#include <cstring>
+#include <unistd.h>
+#include <mosquitto.h>
+
+// using namespace rapidjson;
 // maeusing namespace std;
-#define DEFAULT_VERSION_PATH "/opt/version"
-#define APP_NAME "WeighBox"
-#define CONFIG_NAME "WeighBox.ini"
+
+// MQTT服务器地址和端口号
+#define MQTT_HOST "192.168.80.132"
+#define MQTT_PORT 1883
+
+// MQTT客户端ID
+#define MQTT_CLIENT_ID "mqtt_cpp_client"
+
+// MQTT主题
+#define MQTT_TOPIC "test_topic"
+
+// 消息内容
+#define MQTT_MESSAGE "Hello from MQTT C++ Client"
+
+void on_connect(struct mosquitto *mosq, void *userdata, int result)
+{
+    if (result == 0)
+    {
+        std::cout << "Connected to MQTT server" << std::endl;
+        mosquitto_publish(mosq, NULL, MQTT_TOPIC, strlen(MQTT_MESSAGE), MQTT_MESSAGE, 0, false);
+    }
+    else
+    {
+        std::cerr << "Failed to connect to MQTT server" << std::endl;
+    }
+}
 
 int main()
 {
-    COUT << "=========WeighBox=============" << VERSION << endl;
+    // COUT << "=========WeighBox=============" << VERSION << endl;
+    struct mosquitto *mosq;
+
+    mosquitto_lib_init();
+    mosq = mosquitto_new(MQTT_CLIENT_ID, true, NULL);
+
+    mosquitto_connect_callback_set(mosq, on_connect);
+
+    if (mosquitto_connect(mosq, MQTT_HOST, MQTT_PORT, 60) != MOSQ_ERR_SUCCESS)
+    {
+        std::cerr << "Unable to connect to MQTT server" << std::endl;
+        return 1;
+    }
+
+    mosquitto_loop_start(mosq);
+
+    // 等待连接和消息发布
+    sleep(5);
+
+    mosquitto_disconnect(mosq);
+    mosquitto_destroy(mosq);
+    mosquitto_lib_cleanup();
+
     return 0;
 }
